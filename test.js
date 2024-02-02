e141d77834a90d53993be996f3d7816ccadcf1ea
ftroisi <f.troisi@infos.it>
2024-02-02 23:58:41 +0100

    minor fix

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
-// helloWorldScript.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Create a new div element
-    var divElement = document.createElement("div");
+// Create a new div element
+var divElement = document.createElement("div");
 
-    // Set the text content of the div
-    divElement.textContent = "Hello, World!";
+// Set the text content of the div
+divElement.textContent = "Hello, World!";
 
-    // Append the div to the body of the HTML document
-    document.body.appendChild(divElement);
-});
-
+// Append the div to the body of the HTML document
+document.body.appendChild(divElement);
