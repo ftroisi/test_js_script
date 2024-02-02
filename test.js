0043b1994da5e8d1f40f79fdec834edfc98e9f99
ftroisi <f.troisi@infos.it>
2024-02-03 00:38:03 +0100

    debug

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+console.log("Starting script: Hello, World!");
 // Create a new div element
 var divElement = document.createElement("div");
+console.log("Created a new div element", divElement);
 
 // Set the text content of the div
 divElement.textContent = "Hello, World!";
